c1f956ff587df6e7a7067372d841ceb9170d2059
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-10-29 17:47:26 +0200

    Initialize project structure

---
+++ src/main/java/com/github/tmd/gamelog/GameLogApplication.java
@@ -0,0 +1,13 @@
+package com.github.tmd.gamelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLogApplication.class, args);
+	}
+
+}
