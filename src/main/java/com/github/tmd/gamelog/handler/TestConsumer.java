5416c8317a15bf2cbcc6e3a00f08d9f801e6a1ab
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-11-07 14:33:39 +0100

    Add simple counter for the test message

--- src/main/java/com/github/tmd/gamelog/handler/TestConsumer.java
+++ src/main/java/com/github/tmd/gamelog/handler/TestConsumer.java
@@ -1,10 +1,13 @@
 package com.github.tmd.gamelog.handler;
 
 import com.github.tmd.gamelog.model.TestMessage;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -12,6 +15,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class TestConsumer {
+
+  private final MeterRegistry meterRegistry;
+  private final Counter testCounter;
+
+  @Autowired
+  public TestConsumer(MeterRegistry meterRegistry) {
+    this.meterRegistry = meterRegistry;
+    this.testCounter = this.meterRegistry.counter("test");
+  }
+
   @KafkaListener(topics = "test")
   public void logTest(ConsumerRecord<String, TestMessage> record, @Payload TestMessage str) {
     var key = record.key();
@@ -20,5 +33,6 @@ public class TestConsumer {
         .map(header -> new String(header.value())).collect(Collectors.toUnmodifiableList());
     log.info("Received player event - key {}: Headers [{}] | Payload: {} | Record: {}", key,
         headers, payload, record);
+    testCounter.increment();
   }
 }
