1c69517a568ab3409821d769c8f356fb45d2e0ff
leonbartz <leonbartz@icloud.com>
2021-12-14 14:59:10 +0100

    fix test testing wrong class

--- src/test/java/com/github/tmd/gamelog/RoundScoreRepositoryTest.java
+++ src/test/java/com/github/tmd/gamelog/RoundScoreRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.github.tmd.gamelog;
 
+import com.github.tmd.gamelog.adapter.jpa.RoundScoreDto;
 import com.github.tmd.gamelog.adapter.jpa.RoundScoreJpsRepository;
 
 import com.github.tmd.gamelog.adapter.jpa.RoundScoreRepository;
@@ -27,16 +28,16 @@ public class RoundScoreRepositoryTest {
 
     @Test
     void testFindByGameAndRoundAndPlayerNotExisting() {
-        Mockito.when(roundScoreRepository.findByGameAndRoundAndPlayer("1", "2", "3")).thenReturn(null);
+        Mockito.when(roundScoreJpsRepository.findByGameAndRoundAndPlayer("1", "2", "3")).thenReturn(null);
         RoundScore roundScore = roundScoreRepository.findByGameAndRoundAndPlayer("1", "2", "3");
         assert roundScore.getMovementScore() == 0;
     }
 
     @Test
     void testFindByGameAndRoundAndPlayerExisting() {
-        RoundScore mockRoundScore = new RoundScore();
-        mockRoundScore.setMovementScore(1);
-        Mockito.when(roundScoreRepository.findByGameAndRoundAndPlayer("1", "2", "3")).thenReturn(mockRoundScore);
+        RoundScoreDto mockRoundScoreDto = new RoundScoreDto();
+        mockRoundScoreDto.setMovementScore(1);
+        Mockito.when(roundScoreJpsRepository.findByGameAndRoundAndPlayer("1", "2", "3")).thenReturn(mockRoundScoreDto);
         RoundScore roundScore = roundScoreRepository.findByGameAndRoundAndPlayer("1", "2", "3");
         assert roundScore.getMovementScore() == 1;
     }
