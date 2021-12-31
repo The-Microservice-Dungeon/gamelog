098a4f8c42373f893b4221c8bf1bf61cbf7cfae6
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-12-31 15:00:07 +0100

    feat: add game search by round id

--- src/main/java/com/github/tmd/gamelog/adapter/jpa/game/GameRepositoryImpl.java
+++ src/main/java/com/github/tmd/gamelog/adapter/jpa/game/GameRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.github.tmd.gamelog.adapter.jpa.game;
 import com.github.tmd.gamelog.domain.game.Game;
 import com.github.tmd.gamelog.domain.game.Game.GameId;
 import com.github.tmd.gamelog.domain.game.GameRepository;
+import com.github.tmd.gamelog.domain.game.Round.RoundId;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
@@ -28,6 +29,12 @@ public class GameRepositoryImpl implements GameRepository {
   @Override
   public Optional<Game> findGameById(GameId gameId) {
     var found = gameJpaRepository.findById(gameId.gameId());
+    return found.map(this.gameMapper::toDomain);
+  }
+
+  @Override
+  public Optional<Game> findGameByRoundId(RoundId roundId) {
+    var found = gameJpaRepository.findGameJpaByRounds_RoundId(roundId.roundId());
     return found.map(this.gameMapper::toDomain);
   }
 }
