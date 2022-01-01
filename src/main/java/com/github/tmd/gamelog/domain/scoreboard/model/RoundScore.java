648dbf357a2ef4989dff600f2bd535121ef149c9
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-01 16:35:06 +0100

    chore: add refactoring comment

--- src/main/java/com/github/tmd/gamelog/domain/scoreboard/model/RoundScore.java
+++ src/main/java/com/github/tmd/gamelog/domain/scoreboard/model/RoundScore.java
@@ -3,4 +3,5 @@ package com.github.tmd.gamelog.domain.scoreboard.model;
 import com.github.tmd.gamelog.domain.scoreboard.model.Round;
 import lombok.Data;
 
+// TODO: This should be a map Map<Round, RoundScore>
 public record RoundScore(Round round) { }
