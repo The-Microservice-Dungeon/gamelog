973d074901e3920842811d947c9ca225093013b1
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-01 17:00:50 +0100

    refactor: aggregator

--- src/main/java/com/github/tmd/gamelog/domain/scoreboard/model/PlayerGameScore.java
+++ src/main/java/com/github/tmd/gamelog/domain/scoreboard/model/PlayerGameScore.java
@@ -6,4 +6,11 @@ import lombok.Data;
 @Data
 public class PlayerGameScore {
   private final Map<Round, RoundScore> roundScores;
+
+  public void addRoundScore(Round round, RoundScore roundScore) {
+    if(roundScores.containsKey(round) && roundScores.get(round) != null) {
+      throw new RuntimeException("Cannot update existing round score");
+    }
+    roundScores.put(round, roundScore);
+  }
 }
