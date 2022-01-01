30735312b36663a73ce9a98d382a054535f60156
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-01 01:28:12 +0100

    chore: disable h2 profile

--- src/test/java/com/github/tmd/gamelog/adapter/jpa/scoreboard/ScoreboardRepositoryImplTest.java
+++ src/test/java/com/github/tmd/gamelog/adapter/jpa/scoreboard/ScoreboardRepositoryImplTest.java
@@ -22,7 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("h2")
+//@ActiveProfiles("h2")
 class ScoreboardRepositoryImplTest {
 
   @Autowired
