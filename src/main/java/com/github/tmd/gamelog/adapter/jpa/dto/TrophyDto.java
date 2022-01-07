a4c320cffa70e12c38505a26490fb09e489f947c
ahahn94 <ahahn94@outlook.com>
2022-01-07 18:44:56 +0100

    Refine Columns Of TrophyDto
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- src/main/java/com/github/tmd/gamelog/adapter/jpa/dto/TrophyDto.java
+++ src/main/java/com/github/tmd/gamelog/adapter/jpa/dto/TrophyDto.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * DTO class for persistent storage of the trophies.
@@ -21,9 +18,13 @@ public class TrophyDto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "trophy_id", updatable = false, nullable = false)
     private Long id;
+    @Column(name = "name", updatable = true, nullable = false, unique = true)
     private String name;
+    @Column(name = "badge_url", updatable = true, nullable = false)
     private String badgeUrl;
+    @Column(name = "trophy_type", updatable = false, nullable = false, unique = true)
     private TrophyType trophyType;
 
 }
