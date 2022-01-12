f6a19d363add29cfb25de1a4394d3e4c1780bba9
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-12 16:39:48 +0100

    Further more abstraction
    Dunno if that's useful - but hey, it's java so abstraction is good.

--- src/main/java/com/github/tmd/gamelog/application/score/core/AbstractCategorizedRoundScoreAccumulator.java
+++ src/main/java/com/github/tmd/gamelog/application/score/core/AbstractCategorizedRoundScoreAccumulator.java
@@ -6,6 +6,11 @@ import com.github.tmd.gamelog.domain.score.core.ScoreCategory;
 import java.util.Map;
 import java.util.UUID;
 
-public abstract class AbstractCategorizedRoundScoreAccumulator<T extends CategorizableScore> {
+public abstract class AbstractCategorizedRoundScoreAccumulator<T extends CategorizableScore> extends AbstractScoreAccumulator<T> {
   public abstract Map<UUID, T> accumulateRoundScores(UUID roundId);
+
+  @Override
+  public Map<UUID, T> accumulateScoreInRound(UUID roundId) {
+    return accumulateRoundScores(roundId);
+  }
 }
