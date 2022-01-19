218166a8db503a009c6b00ee1fed5e0fa7ece01d
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-19 11:37:08 +0100

    use static uuids

--- src/test/resources/db/mock_history.sql
+++ src/test/resources/db/mock_history.sql
@@ -1,39 +1,39 @@
-SET @player1_id = uuid();
-SET @player2_id = uuid();
-SET @player3_id = uuid();
+SET @player1_id = '7228d445-0f83-4ac8-888a-e3d70314059d';
+SET @player2_id = '417d25b8-1a78-49f4-855c-2091403d389b';
+SET @player3_id = 'c5e8838c-dffd-4282-b9f4-58ce2996bf9c';
 
-SET @game_id = uuid();
+SET @game_id = '22838d5f-3e11-46fd-9074-d76d0e2ad27c';
 
-SET @round1_id = uuid();
-SET @round2_id = uuid();
-SET @round3_id = uuid();
+SET @round1_id = 'a03cea0f-1a6b-4127-973a-658839913225';
+SET @round2_id = 'fb4d537b-61e1-4165-ad01-0745504a1955';
+SET @round3_id = 'c0305195-0005-4ee9-8350-023ee9e588d0';
 
-SET @player1_transaction1 = uuid();
-SET @player1_transaction2 = uuid();
-SET @player1_transaction3 = uuid();
-SET @player1_transaction4 = uuid();
-SET @player1_transaction5 = uuid();
+SET @player1_transaction1 = '1e479e4c-ce06-4119-962c-25a561087622';
+SET @player1_transaction2 = '17698f28-75a3-4016-9202-ec5c8d91efc3';
+SET @player1_transaction3 = 'ec78782d-5814-462d-af0e-143df6c53df0';
+SET @player1_transaction4 = '282d4f9c-e277-449d-bdaa-14ef8c07b1f5';
+SET @player1_transaction5 = '2902b418-d91e-42d2-b83c-27f42ac807d9';
 
-SET @player1_robot1 = uuid();
-SET @player1_robot2 = uuid();
+SET @player1_robot1 = 'fbe303cd-679b-4e48-bec8-7484dc0b60a7';
+SET @player1_robot2 = '26d7e840-cc0b-4e4f-91ce-e1d9562ff316';
 
-SET @player2_transaction1 = uuid();
-SET @player2_transaction2 = uuid();
-SET @player2_transaction3 = uuid();
-SET @player2_transaction4 = uuid();
-SET @player2_transaction5 = uuid();
+SET @player2_transaction1 = 'b35dec65-4352-43ff-9d53-d0fe64af8429';
+SET @player2_transaction2 = '3e72fc6c-6db1-4361-8b00-88be18edc39b';
+SET @player2_transaction3 = '83952983-b23a-4ce7-8110-a390f5f97384';
+SET @player2_transaction4 = 'a29a0648-594d-4ddf-abee-9fd38dd8a129';
+SET @player2_transaction5 = 'cd70d8b1-55d8-4b7d-b7a9-8e740a39a6d8';
 
-SET @player2_robot1 = uuid();
-SET @player2_robot2 = uuid();
+SET @player2_robot1 = '72c96fd6-a4d5-409b-aab0-6f6d38553228';
+SET @player2_robot2 = 'b75c01ad-0833-40b2-8345-640221e11e4c';
 
-SET @player3_transaction1 = uuid();
-SET @player3_transaction2 = uuid();
-SET @player3_transaction3 = uuid();
-SET @player3_transaction4 = uuid();
-SET @player3_transaction5 = uuid();
+SET @player3_transaction1 = 'f64acba8-2958-4809-ad59-721005d536cf';
+SET @player3_transaction2 = '0f10786e-6f83-4416-bb57-9e28694e484e';
+SET @player3_transaction3 = '28051134-ab96-4348-93b7-3753cf840298';
+SET @player3_transaction4 = '2926d821-23d6-4180-b48f-78c28faf29c0';
+SET @player3_transaction5 = 'a1a53f5e-ae29-430a-aa06-a7530af484ea';
 
-SET @player3_robot1 = uuid();
-SET @player3_robot2 = uuid();
+SET @player3_robot1 = '06ca3a1c-5232-4525-8509-f882a782091d';
+SET @player3_robot2 = 'feeba2e1-b9dc-40cc-850d-29d42dc570e6';
 
 /* We assume to have only one running game at a time */
 insert into game_status_history_jpa(id, created_at, game_id, status, TIMESTAMP)
