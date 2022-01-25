ede048236b825a977679430065b7fce2ff9a4e8e
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-25 19:20:47 +0100

    Fix tests as default trophies are initialized now

--- src/test/java/com/github/tmd/gamelog/adapter/jpa/TrophyRepositoryTest.java
+++ src/test/java/com/github/tmd/gamelog/adapter/jpa/TrophyRepositoryTest.java
@@ -20,43 +20,45 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 public class TrophyRepositoryTest {
 
-    private final String name = "First Blood";
-    private final String badgeUrl = "https://raw.githubusercontent.com/wiki/The-Microservice-Dungeon/gamelog/assets/pictures/trophies/achievements/Fighting%20Bronze%20-%20First%20Blood.png";
-    @Autowired
-    private TrophyJpaRepository trophyJpaRepository;
-    private TrophyDtoMapper trophyDtoMapper;
-    private TrophyRepository trophyRepository;
+  private final String name = "First Blood (Test Trophy)";
+  private final String badgeUrl = "https://raw.githubusercontent.com/wiki/The-Microservice-Dungeon/gamelog/assets/pictures/trophies/achievements/Fighting%20Bronze%20-%20First%20Blood.png";
+  @Autowired
+  private TrophyJpaRepository trophyJpaRepository;
+  private TrophyDtoMapper trophyDtoMapper;
+  private TrophyRepository trophyRepository;
 
-    @BeforeEach
-    void beforeEach() {
-        trophyDtoMapper = new TrophyDtoMapper();
-        trophyRepository = new TrophyRepository(trophyJpaRepository, trophyDtoMapper);
-    }
+  @BeforeEach
+  void beforeEach() {
+    trophyDtoMapper = new TrophyDtoMapper();
+    trophyRepository = new TrophyRepository(trophyJpaRepository, trophyDtoMapper);
+  }
 
-    @Test
-    void testUpsertAndFindAll() {
-        Trophy testTrophy = new Trophy();
-        testTrophy.setName(name);
-        testTrophy.setBadgeUrl(badgeUrl);
-        assertThat(trophyRepository.findAll()).isEmpty();
-        trophyRepository.upsert(testTrophy);
-        ArrayList<Trophy> trophies = trophyRepository.findAll();
-        assertThat(trophies.get(0)).usingRecursiveComparison().ignoringFields("id").isEqualTo(testTrophy);
-    }
+  @Test
+  void testUpsertAndFindAll() {
+    Trophy testTrophy = new Trophy();
+    testTrophy.setName(name);
+    testTrophy.setBadgeUrl(badgeUrl);
+    assertThat(trophyRepository.findAll()).hasSize(Trophy.getDefaultTrophies().size());
+    trophyRepository.upsert(testTrophy);
+    ArrayList<Trophy> trophies = trophyRepository.findAll();
+    assertThat(trophies.stream().anyMatch(
+        t -> t.getName().equals(testTrophy.getName()) && t.getBadgeUrl()
+            .equals(testTrophy.getBadgeUrl()))).isTrue();
+  }
 
-    @Test
-    void testInitRepository() {
-        // Prepare repository with at least one trophy.
-        trophyRepository.upsert(new FightingFirstPlaceTrophy());
-        assertThat(trophyRepository.findAll()).isNotEmpty();
+  @Test
+  void testInitRepository() {
+    // Prepare repository with at least one trophy.
+    assertThat(trophyRepository.findAll()).isNotEmpty();
 
-        // Run the init function.
-        trophyRepository.initRepository();
+    // Run the init function.
+    trophyRepository.initRepository();
 
-        // Check if the list of trophies on the repo matches the list of default trophies (ignoring IDs and order).
-        ArrayList<Trophy> requiredTrophies = Trophy.getDefaultTrophies();
-        ArrayList<Trophy> trophiesOnTheRepository = trophyRepository.findAll();
-        assertThat(trophiesOnTheRepository).usingRecursiveComparison().ignoringFields("id").ignoringCollectionOrder().isEqualTo(requiredTrophies);
-    }
+    // Check if the list of trophies on the repo matches the list of default trophies (ignoring IDs and order).
+    ArrayList<Trophy> requiredTrophies = Trophy.getDefaultTrophies();
+    ArrayList<Trophy> trophiesOnTheRepository = trophyRepository.findAll();
+    assertThat(trophiesOnTheRepository).usingRecursiveComparison().ignoringFields("id")
+        .ignoringCollectionOrder().isEqualTo(requiredTrophies);
+  }
 
 }
