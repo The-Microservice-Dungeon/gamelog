db780f8bb70671d0f1b9f36cdea6b4f070678a89
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-25 19:36:58 +0100

    Add json alias for player-id

--- src/main/java/com/github/tmd/gamelog/adapter/rest/client/response/PlayerBalanceEntryDto.java
+++ src/main/java/com/github/tmd/gamelog/adapter/rest/client/response/PlayerBalanceEntryDto.java
@@ -1,8 +1,9 @@
 package com.github.tmd.gamelog.adapter.rest.client.response;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
 
 public record PlayerBalanceEntryDto(
-    @JsonProperty("player-id") UUID playerId,
+    @JsonAlias({ "player-id "}) @JsonProperty("playerId") UUID playerId,
     @JsonProperty("balance") Integer balance) { }
--- src/main/java/com/github/tmd/gamelog/adapter/rest/client/response/PlayerBalanceRoundEntryDto.java
+++ src/main/java/com/github/tmd/gamelog/adapter/rest/client/response/PlayerBalanceRoundEntryDto.java
@@ -1,9 +1,10 @@
 package com.github.tmd.gamelog.adapter.rest.client.response;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
 
 public record PlayerBalanceRoundEntryDto(
     @JsonProperty("round") Integer roundNumber,
-    @JsonProperty("playerId") UUID playerId,
+    @JsonAlias({ "player-id "}) @JsonProperty("playerId") UUID playerId,
     @JsonProperty("balance") Integer balance) { }
