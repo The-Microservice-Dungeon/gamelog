9f8bb895572076c306a8c42533e0b606ecb4fcf5
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-26 21:00:51 +0100

    add lowercase support for resource type

--- src/main/java/com/github/tmd/gamelog/adapter/event/gameEvent/map/ResourceType.java
+++ src/main/java/com/github/tmd/gamelog/adapter/event/gameEvent/map/ResourceType.java
@@ -1,11 +1,23 @@
 package com.github.tmd.gamelog.adapter.event.gameEvent.map;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum ResourceType {
+  @JsonAlias({"coal"})
+  @JsonProperty("COAL")
   COAL,
+  @JsonAlias({"iron"})
+  @JsonProperty("IRON")
   IRON,
+  @JsonAlias({"gem"})
+  @JsonProperty("GEM")
   GEM,
+  @JsonAlias({"gold"})
+  @JsonProperty("GOLD")
   GOLD,
+  @JsonAlias({"platin"})
+  @JsonProperty("PLATIN")
   PLATIN;
 }
